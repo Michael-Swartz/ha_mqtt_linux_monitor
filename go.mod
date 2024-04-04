02d572c729d05bd5446d8ad4b74ede9ade60fe95
michael.swartz <michael.swartz@maymobility.com>
2024-04-04 08:33:02 -0400

    add disk usage

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/mackerelio/go-osstat v0.2.4 // indirect
+	github.com/ricochet2200/go-disk-usage/du v0.0.0-20210707232629-ac9918953285 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 	"strconv"
+	"syscall"
 
 	"github.com/mackerelio/go-osstat/cpu"
 	"github.com/mackerelio/go-osstat/memory"
@@ -20,9 +21,38 @@ var (
 	}
 )
 
+type DiskStats struct {
+	All uint64
+	Free uint64
+	Used uint64
+	Used_Percent float64
+}
+
+func GetDiskUsage(path string) (disk DiskStats) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+
+	disk.Used_Percent = float64(disk.Used)/float64(disk.All)
+	return
+}
+
 func FloatToString(input float64) string {
 	return strconv.FormatFloat(input, 'f', 2, 64)
 }
+
+const (
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
+)
 
 func main() {
 
@@ -48,7 +78,6 @@ func main() {
 		}
 		total := float64(after.Total - before.Total)
 		total_cpu_usage := (float64(float64(after.System-before.System)+float64(after.User-before.User)) / total * 100)
-		//fmt.Printf("cpu total: %f%%\n", (float64(float64(after.System-before.System)+float64(after.User-before.User)) / total * 100))
 		fmt.Printf("cpu total: %s%%\n", FloatToString(total_cpu_usage))
 		token := client.Publish("/test/cpu", 0, false, FloatToString(total_cpu_usage))
 		token.Wait()
@@ -59,8 +88,15 @@ func main() {
 		}
 		total_mem_usage := (float64(memory.Used)/float64(memory.Total)*100)
 		fmt.Printf("memory total use: %s%%\n", FloatToString(total_mem_usage))
-		tokenn := client.Publish("/test/memory", 0, false, FloatToString(total_mem_usage))
-		tokenn.Wait()
+		token_mem := client.Publish("/test/memory", 0, false, FloatToString(total_mem_usage))
+		token_mem.Wait()
+
+		disk := GetDiskUsage("/")
+		total_disk_usage := (float64(disk.Used_Percent)*100)
+		fmt.Printf("Disk Usage: %s%%\n", FloatToString(total_disk_usage))
+		token_disk := client.Publish("/test/disk", 0, false, FloatToString(total_disk_usage))
+		token_disk.Wait()
 		time.Sleep(time.Duration(1) * time.Second)
+
 	}
 }
